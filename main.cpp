81522f38fd06090d9a5bf2cd9e8364c37483dcf9
VladyslavBychok <bychok197@ukr.net>
2021-08-23 01:43:58 +0300

    init

--- main.cpp
+++ main.cpp
@@ -16,33 +16,92 @@ class OneBall {
 private:
 	sf::Vector2f curr;
 	sf::Vector2f dir;
-	Texture TextureBall;
-	Sprite Ball;
+	static Texture text;
+	static Sprite sprite;
 	void calcDir(sf::Vector2f mousePos) {
-		auto ap = mousePos - curr;
-		auto len = sqrtf( ap.x * ap.x + ap.y * ap.y );
-		auto apn = sf::Vector2f( ap.x / len, ap.y / len );
+		auto len = sqrtf(mousePos.x * mousePos.x + mousePos.y * mousePos.y );
+		auto apn = sf::Vector2f(mousePos.x / len, mousePos.y / len );
 		dir = apn;
 	}
+
 public:
 	OneBall(sf::Vector2f curr, const sf::RenderWindow& wnd) {
-		TextureBall.loadFromFile("textures/Ball.png");
-		Ball.setTexture(TextureBall);
+		if (static bool isInit = false; !isInit) {
+			text.loadFromFile("textures/Ball.png");
+			sprite.setTexture(text);
+			isInit = true;
+		}
 
-		this->curr = curr;
-		calcDir( getCursorPosFromHero( curr , wnd));
+		this->curr = curr + sf::Vector2f(8.f, 8.f);
+		calcDir( getCursorPosFromHero( curr , wnd) - sf::Vector2f(16.f, 16.f));
 	}
 
 	void drawAndUpdate(RenderWindow &wnd) {
-		//for (int l = 0; l < coordinates.size(); l++) {
-		//
-		//	Ball.setPosition(coordinates[l].x, coordinates[l].y);
-		//	wnd.draw(Ball);
-		//}
-		Ball.setPosition(curr);
-		wnd.draw(Ball);
-		curr += dir;
+		sprite.setPosition(curr);
+		wnd.draw(sprite);
+		curr += dir / 5.f;
 	}
+};
+Texture OneBall::text;
+Sprite OneBall::sprite;
+
+class Player {
+public:
+	Player() {
+		x_ = y_ = 0.f;
+	}
+
+	void create(const std::string  addressTexture, const int recLeft, const int recTop, const int recWidth, const int recHeight) {
+		texture_.loadFromFile(addressTexture);
+		sprite_.setTexture(texture_);
+		sprite_.setTextureRect(IntRect(recLeft, recTop, recWidth, recHeight));
+	}
+	
+
+
+private:
+	float x_;
+	float y_;
+	Texture texture_;
+	Sprite sprite_;
+};
+
+class Delay {
+public:
+	Delay() {
+		LastTime = 0u;
+	}
+
+	void TimePause(bool& attack, std::vector<OneBall>& balls, Sprite& Axeman, sf::RenderWindow& window) {
+		if (clock() - LastTime >= 80) {
+			attack = true;
+			balls.push_back(OneBall(Axeman.getPosition(), window));
+			LastTime = clock();
+		}
+	}
+
+	void TimePause(bool& attack, int& j) {
+		if (clock() - LastTime >= 80) {
+			LastTime = clock();
+			j++;
+
+			if (j >= 5) {
+				j = 0;
+				attack = false;
+			}
+		}
+	}
+
+	void TimePause(int& i) {
+		if (clock() - LastTime >= 80) {
+			i++;
+			if (i == 5) i = 0;
+			LastTime = clock();
+		}
+	}
+
+private:
+	clock_t LastTime;
 };
 
 enum Direction {
@@ -62,10 +121,13 @@ void print(const sf::Vector2<T> vec) {
 int main() {
 	RenderWindow wnd(VideoMode(1000, 1000), "Game");
 	wnd.setView(View(FloatRect(0,0,200,200)));
+
+	//Player AxemanHero;
+	//AxemanHero.create("textures/Axeman.png", 0, 0, 32, 32);
+
 	Texture TextureAxeman;
 	TextureAxeman.loadFromFile("textures/Axeman.png");
 	
-
 	Sprite Axeman;
 	Axeman.setTexture(TextureAxeman);
 	Axeman.setTextureRect(IntRect(0,0,32,32));
@@ -73,15 +135,19 @@ int main() {
 	std::vector<OneBall> balls;
 
 
-	float x = 0, y = 0, BallX = 0, BallY = 0;
+	float x = 0, y = 0;
 	int i = 0, j = 0;
 	clock_t lastTime = 0u;
 	bool attack = false, BallSpawn = false;
 	
-	std::vector<sf::Vector2f> coordinates;
-
 	Direction last = DOWN;
 
+	/*
+	структурировать код
+	удалять элементы вектора
+	добавить противника, убивать его
+	*/
+	Delay Pause;
 	while (wnd.isOpen()) {
 		Event event;
 
@@ -89,21 +155,15 @@ int main() {
 			if (event.type == Event::EventType::Closed) wnd.close();
 		}
 
-		
-
 		wnd.clear(Color(34,139,34));
-
+		
 		if(Mouse::isButtonPressed(Mouse::Button::Left)){
-			auto position = Mouse::getPosition(wnd);
-			if (clock() - lastTime >= 80) {
-				attack = true;
-				std::cout << "x = " << position.x - Axeman.getPosition().x << "y = " << position.y - Axeman.getPosition().y << std::endl;
-				
-				balls.push_back( OneBall(Axeman.getPosition(), wnd) );
-
-				
+			Pause.TimePause(attack, balls, Axeman, wnd);
+			/*if (clock() - lastTime >= 80) {//////////////////////////////////////////////////////////////////////////////
+				attack = true;				
+				balls.push_back( OneBall(Axeman.getPosition(), wnd) );				
 				lastTime = clock();
-			}
+			}*/
 		}		
 
 		if (attack) {
@@ -124,7 +184,8 @@ int main() {
 			}
 
 			//std::this_thread::sleep_for(std::chrono::milliseconds(50));
-			if (clock() - lastTime >= 80) {
+			Pause.TimePause(attack, j);
+			/*if (clock() - lastTime >= 80) {
 				lastTime = clock();
 				j++;
 
@@ -132,58 +193,62 @@ int main() {
 					j = 0;
 					attack = false;
 				}
-			}
+			}*/
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
 			x -= 0.1f;
 			Axeman.setTextureRect(IntRect(32 * i, 32, 32, 32));
 			last = LEFT;
-			if (clock() - lastTime >= 80) {
+			Pause.TimePause(i);
+			/*if (clock() - lastTime >= 80) {
 				i++;
 				if (i == 5) i = 0;
 				lastTime = clock();
-			}
+			}*/
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
 			x += 0.1f;
 			Axeman.setTextureRect(IntRect(32 * i, 64, 32, 32));
 			last = RIGHT;
-			if (clock() - lastTime >= 80) {
+			Pause.TimePause(i);
+			/*if (clock() - lastTime >= 80) {
 				i++;
 				if (i == 5) i = 0;
 				lastTime = clock();
-			}
+			}*/
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::W)) {
 			y -= 0.1f;
 			Axeman.setTextureRect(IntRect(32 * i, 96, 32, 32));
 			last = UP;
-			if (clock() - lastTime >= 80) {
+			Pause.TimePause(i);
+			/*if (clock() - lastTime >= 80) {
 				i++;
 				if (i == 5) i = 0;
 				lastTime = clock();
-			}
+			}*/
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
 			y += 0.1f;
 			Axeman.setTextureRect(IntRect(32*i, 0, 32, 32));
 			last = DOWN;
-			if (clock() - lastTime >= 80) {
+			Pause.TimePause(i);
+			/*if (clock() - lastTime >= 80) {
 				i++;
 				if (i == 5) i = 0;
 				lastTime = clock();
-			}
+			}*/
 		}
 
 		Axeman.setPosition(x,y);
 		wnd.draw(Axeman);
 
 
-		for (auto& ball : balls) {
+		for (auto& ball : balls) {//foreach
 			ball.drawAndUpdate(wnd);
 		}
 
